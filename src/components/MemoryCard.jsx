1697b6e8bf79bd08a03884035ff7055ff7661c7e
Nate Lee <nleepercussion@gmail.com>
2021-07-21 11:15:14 -0400

    Part One finished

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MemoryCard.css';
+
+class MemoryCard extends React.Component {
+    render() {
+        return (
+            <div className="MemoryCard">
+                <img src="http://digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="dc logo"></img>
+            </div>
+        )
+    }
+}
+
+export default MemoryCard;
