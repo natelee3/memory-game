47ed9a5e086eeb2b523b5602e7f963a4f933e18e
Nate Lee <nleepercussion@gmail.com>
2021-07-21 16:02:39 -0400

    Finished part 3. Refactored with emojis

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -3,9 +3,17 @@ import './MemoryCard.css';
 
 class MemoryCard extends React.Component {
     render() {
+        let memoryCardInnerClass = this.props.isFlipped ? "MemoryCardInner flipped" : "MemoryCardInner";
         return (
-            <div className="MemoryCard">
-                <img src="http://digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="dc logo"></img>
+            <div className="MemoryCard" onClick={this.props.pickCard}>
+                <div className={memoryCardInnerClass}>
+                    <div className="MemoryCardBack">
+                        <img src="http://digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="dc logo"></img>
+                    </div>
+                    <div className="MemoryCardFront">
+                      {this.props.symbol}
+                    </div>
+                </div>                
             </div>
         )
     }
